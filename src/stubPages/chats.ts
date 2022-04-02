38444de09d13af11e79f4869b7fe15010d835fd7
Anton Lukyanov <lukyanovanton13@yandex.ru>
2022-04-02 11:16:32 +0600

    *

--- src/stubPages/chats.ts
+++ src/stubPages/chats.ts
@@ -1,26 +1,26 @@
 import '../styles/app.css'
 
-import {renderDOM, registerComponent} from '../helpers';
+import {renderDOM, registerComponent, registerDateHelper} from '../helpers';
 
 import ChatsPage from '../pages/chats';
 import Placeholder from '../pages/chats/components/placeholder';
 import ChatList from '../pages/chats/components/chatList';
 import ChatInfo from '../pages/chats/components/chatList/chatInfo';
 import ChatSearch from '../components/SearchField';
-import Chat from '../pages/chats/components/chat';
-import DailyMessageRibbon from '../pages/chats/components/chat/dailyMessageRibbon';
+import ChatBlock from '../pages/chats/components/chat';
+import DailyMessageRibbonBlock from '../pages/chats/components/chat/dailyMessageRibbon';
 import MessageBlock from '../pages/chats/components/chat/message';
 import NewMessage from '../pages/chats/components/chat/newMessage';
 import Input from '../components/Input';
 import BaseButton from '../components/BaseButton';
 
-
+registerDateHelper();
 registerComponent(Placeholder);
 registerComponent(ChatList);
 registerComponent(ChatInfo);
 registerComponent(ChatSearch);
-registerComponent(Chat);
-registerComponent(DailyMessageRibbon);
+registerComponent(ChatBlock);
+registerComponent(DailyMessageRibbonBlock);
 registerComponent(MessageBlock);
 registerComponent(NewMessage);
 registerComponent(Input);
