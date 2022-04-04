b30e20adb1bbeec1a4b8ee4c5a6931e83c9faaae
Anton Lukyanov <lukyanovanton13@yandex.ru>
2022-04-04 23:20:08 +0600

    Валидация email

--- src/modules/validation/email.ts
+++ src/modules/validation/email.ts
@@ -2,9 +2,8 @@ import { ValidationResult } from "./types";
 import {required} from './common';
 export const validate=(email:string):ValidationResult=>{
     if(required(email).isFailure)
-        return ValidationResult.fail("Требуется почта.");
-    
-    if(!/(?!^\d+$)^[a-zA-Z0-9-_]+$/.test(email))
+        return ValidationResult.fail("Требуется почта.");    
+    if(!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email))
         return ValidationResult.fail("Логин может содержать цифры, но не состоять из них, латиница, без пробелов, без спецсимволов (допустимы дефис и нижнее подчёркивание).");
     return ValidationResult.ok();
 }
--- src/modules/validation/name.ts
+++ src/modules/validation/name.ts
@@ -1,9 +1,9 @@
 import { ValidationResult } from "./types";
 import {required} from './common';
-export const validate=(login:string):ValidationResult=>{
-    if(required(login).isFailure)
+export const validate=(name:string):ValidationResult=>{
+    if(required(name).isFailure)
         return ValidationResult.fail("Имя не может быть пустым.");
-   
-    
+    if(!/^[A-ZА-Я]{1}[A-Za-zА-Яа-я-]*$/.test(name))
+        return ValidationResult.fail("Первая буква должна быть заглавной,латиница или кириллица, без пробелов и без цифр, нет спецсимволов (допустим только дефис).");
     return ValidationResult.ok();
 }
