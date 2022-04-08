38531f17891016c3a5e59a8c12f03009659abe9c
alukyanov <alukyanov@gemsdev.com>
2022-04-08 21:35:06 +0600

    Вынес константы в правилах  проверок в именованные переменные

--- src/modules/validation/login.ts
+++ src/modules/validation/login.ts
@@ -1,12 +1,18 @@
 import { ValidationResult } from "./types";
-import {required} from './common';
-export const validate=(login:string):ValidationResult=>{
-    if(required(login).isFailure)
-        return ValidationResult.fail("Требуется логин.");
-    const length=login.length;
-    if(length<3 || length >20)
-        return ValidationResult.fail("Длина логина должна быть от 3 до 20 символов.");
-    if(!/(?!^\d+$)^[\w-]+$/.test(login))
-        return ValidationResult.fail("Логин может содержать цифры, но не состоять из них, латиница, без пробелов, без спецсимволов (допустимы дефис и нижнее подчёркивание).");
-    return ValidationResult.ok();
-}
+import { required } from "./common";
+const minLoginLength = 3;
+const maxLoginLength = 20;
+export const validate = (login: string): ValidationResult => {
+  if (required(login).isFailure)
+    return ValidationResult.fail("Требуется логин.");
+  const length = login.length;
+  if (length < minLoginLength || length > maxLoginLength)
+    return ValidationResult.fail(
+      `Длина логина должна быть от ${minLoginLength} до ${maxLoginLength} символов.`
+    );
+  if (!/(?!^\d+$)^[\w-]+$/.test(login))
+    return ValidationResult.fail(
+      "Логин может содержать цифры, но не состоять из них, латиница, без пробелов, без спецсимволов (допустимы дефис и нижнее подчёркивание)."
+    );
+  return ValidationResult.ok();
+};
--- src/modules/validation/phone.ts
+++ src/modules/validation/phone.ts
@@ -1,12 +1,18 @@
 import { ValidationResult } from "./types";
-import {required} from './common';
-export const validate=(phone:string):ValidationResult=>{
-    if(required(phone).isFailure)
-        return ValidationResult.fail("Требуется телефон.");
-    const length=phone.length;
-    if(length<10 || length >15)
-        return ValidationResult.fail("Длина телефона должна быть от 10 до 15 символов.");
-    if(!/^\+*[0-9]+$/.test(phone))
-        return ValidationResult.fail("Телефон должен состоять из цифр, может начинается с плюса.");
-    return ValidationResult.ok();
-}
+import { required } from "./common";
+const minPhoneLength = 10;
+const maxPhoneLength = 15;
+export const validate = (phone: string): ValidationResult => {
+  if (required(phone).isFailure)
+    return ValidationResult.fail("Требуется телефон.");
+  const length = phone.length;
+  if (length < minPhoneLength || length > maxPhoneLength)
+    return ValidationResult.fail(
+      `Длина телефона должна быть от ${minPhoneLength} до ${maxPhoneLength} символов.`
+    );
+  if (!/^\+*[0-9]+$/.test(phone))
+    return ValidationResult.fail(
+      "Телефон должен состоять из цифр, может начинается с плюса."
+    );
+  return ValidationResult.ok();
+};
