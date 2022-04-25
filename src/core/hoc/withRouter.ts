0f01879af095498412a6666639bb9323d76c7412
alukyanov <alukyanov@gemsdev.com>
2022-04-25 17:57:11 +0600

    fix hoc

--- src/core/hoc/withRouter.ts
+++ src/core/hoc/withRouter.ts
@@ -1,11 +1,17 @@
-import Block from "../Block";
+import { BlockClass2 } from "../Block";
+import BrowserRouter from "../router/BrowserRouter";
 import Router from "../router/BrowserRouter";
 
-export function withRouter<T>(Component: typeof Block) {
-  return class WithRouter extends Component<T> {
-    public static componentName = Component.name;
-    constructor(props: T & { router: Router }) {
+type WithRouterProps = { router: BrowserRouter };
+export function withRouter<P extends WithRouterProps>(
+  WrappedBlock: BlockClass2<P>
+) {
+  // @ts-expect-error No base constructor has the specified number of type arguments
+  return class extends WrappedBlock<P> {
+    public static componentName =
+      WrappedBlock.componentName || WrappedBlock.name;
+    constructor(props: P & { router: Router }) {
       super({ ...props, router: window.router });
     }
-  };
+  } as BlockClass2<Omit<P, "router">>;
 }
