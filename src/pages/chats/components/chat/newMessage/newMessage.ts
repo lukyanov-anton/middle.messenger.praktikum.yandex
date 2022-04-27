81c51a1206fa15c4f161b6107846598b0887322d
alukyanov <alukyanov@gemsdev.com>
2022-04-27 22:16:59 +0600

    fix by code review

--- src/pages/chats/components/chat/newMessage/newMessage.ts
+++ src/pages/chats/components/chat/newMessage/newMessage.ts
@@ -28,8 +28,6 @@ export class NewMessage extends Block {
     const onSubmit = (e: Event) => {
       if (this.validate()) {
         sendMessageToChat(this.props.chatId, this.state.values.message);
-        //this.state.values.message = "";
-        //this.setState(this.state);
         this.setState({
           values: { message: "" },
         });
